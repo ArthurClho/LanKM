432568cb9fd0b2e25badceb214be01a7bd19e763
Arthur Coelho <arthurclho@gmail.com>
2024-10-23 19:59:32 -0300

    LanKM: Add stub for windows input injection

---
+++ lankm/src/input_injection/windows.rs
@@ -0,0 +1,13 @@
+use crate::data::KeyEvent;
+
+pub struct InputInjector {}
+
+impl InputInjector {
+    pub fn new() -> Self {
+        todo!();
+    }
+
+    pub fn emit(&mut self, _e: KeyEvent) {
+        todo!();
+    }
+}
